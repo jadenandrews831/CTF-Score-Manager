5fcc305249f4d54cf85f4b8040068929120bd3cd
Jaden Andrews <59934265+jadenandrews831@users.noreply.github.com>
2024-04-27 13:45:18 -0400

    initial project framework

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const system = require('system-commands')
+
+const app = express()
+
+aapp.listen(3000, () => {
+  console.log("Listening on Port http://localhost:3000");
+});
